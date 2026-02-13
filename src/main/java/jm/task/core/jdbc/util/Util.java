bc4fd3c0645148280959b9657e72fc8bb7a53c98
Igor <mithikg@yandex.ru>
2026-02-13 17:50:51 +0500

    Hibernate01

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -15,7 +15,7 @@ public class Util {
     private static final String URL =
             "jdbc:mysql://localhost:3306/preproject?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD = "Liza07_liza98?";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() {
         try {
